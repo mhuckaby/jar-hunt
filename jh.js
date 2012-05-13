669cb8a7f26b9ecaf6b05d5ecaa5c331080f555e
Matthew Huckaby <matthew.huckaby@gmail.com>
2012-05-12 21:09:25 -0500

    removed global variable usage

--- jh.js
+++ jh.js
@@ -36,27 +36,24 @@ Copyright 2012 Matthew David Huckaby
    limitations under the License.
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 */
-var crypto = require('crypto')
-var events = require('events')
-var fs = require('fs')
-var http = require('http')
-var path = require('path')
-var util = require('util')
-
 var jh = { 	
 	config:{
-		emitter:new events.EventEmitter(),
+		console:console,
+		emitter:null,
 		filenames:{
 			dependency_xml:'dependency.xml',
-			error_xml:'error.xml'
+			error_xml:'error.xml',
+			this_script:__filename
 		},
 		http_options:{  
 			host:'search.maven.org',
 			port:80, 	
 		},
-		max_errors:3,
+		logger:{
+			error:null,
+			info:null
+		},		
 		msgs:{
-			error_exit:'Encountered too many errors to continue.',
 			found_jar:'found, "%s", hash(sha1)=%s',
 			help_text:['jar-hunt, v.1.0.0b', 
 				'usage:', 
@@ -69,7 +66,16 @@ var jh = {
 			validation_not_directory:'argument, "%s" is not a directory',
 			validation_dir_ne:'directory, "%s" does not exist.'
 		},
+		process:process,
 		recursive:false,
+		requires:{
+			crypto:require('crypto'),
+			events:require('events'),
+			fs:require('fs'),
+			http:require('http'),
+			path:require('path'),
+			util:require('util')			
+		},
 		url:{
 			param_template:':"%s"',
 			template:'/solrsearch/select?q=1%s&rows=20&wt=json'
@@ -84,163 +90,147 @@ var jh = {
 				'</dependency>\n'
 		}
 	},
-	state:{
-		error_count:0,
-		errors:[],
-		logger:{
-			error:null,
-			info:null
-		},
-	},
-	error_counter:function(error){
-		jh.state.errors.push(error)
-		if(++jh.state.error_count == jh.config.max_errors){
-			console.log(jh.config.msgs.error_exit)
-			jh.state.errors.forEach(function(e){
-				console.log(util.format('\t%s',e))
-			})
-			process.exit()
-		}
-	},
-	execute:function(dir){
-		fs.readdir(dir, function(err, filenames){
+	execute:function(dir, config){
+		var dir = dir ? dir : config.process.argv[config.process.argv.length-1]
+		config.requires.fs.readdir(dir, function(err, filenames){
 				filenames.forEach(function(filename){					
 					var qualified_filename = (dir ? (dir + '/') : '') + filename
-					jh.config.emitter.emit('filter', qualified_filename)
+					config.emitter.emit('filter', qualified_filename, config)
 				})
 			}
 		)
 		return jh			
 	},
-	filter:function(filename, index, list, regex){
-		fs.stat(filename, function(error, stats){
-			if(jh.config.recursive && stats.isDirectory()){
-				jh.config.emitter.emit('execute_on_dir', filename)
+	filter:function(filename, config){
+		config.requires.fs.stat(filename, function(error, stats){
+			if(config.recursive && stats.isDirectory()){
+				config.emitter.emit('execute_on_dir', filename, config)
 			}else{
-				if(filename.match(regex ? regex : /\.jar$/)){
-					jh.config.emitter.emit('generate_hash', filename)
+				if(filename.match(/\.jar$/)){
+					config.emitter.emit('generate_hash', filename, config)
 				}
 			}
 		})
 	},
-	generate_hash:function(filename){
-		fs.readFile(filename, function(err, data){
-			var hash = crypto.createHash('sha1').update(data).digest('hex')
-			if(jh.config.msgs.found_jar){
-				console.log(util.format(jh.config.msgs.found_jar, filename, hash))
+	generate_hash:function(filename, config){
+		config.requires.fs.readFile(filename, function(err, data){
+			var hash = config.requires.crypto.createHash('sha1').update(data).digest('hex')
+			if(config.msgs.found_jar){
+				config.console.log(config.requires.util.format(config.msgs.found_jar, filename, hash))
 			}
-			jh.config.emitter.emit('generate_url', filename, hash)
+			config.emitter.emit('generate_url', filename, hash, config)
 		})		
 	},
-	generate_url:function(filename, hash){
-		var param = encodeURIComponent(util.format(jh.config.url.param_template, hash))
-		jh.config.emitter.emit('search', filename, util.format(jh.config.url.template, param))
+	generate_url:function(filename, hash, config){
+		var param = encodeURIComponent(config.requires.util.format(jh.config.url.param_template, hash))
+		config.emitter.emit('search', filename, config.requires.util.format(jh.config.url.template, param), config)
 	},
-	initialize:function(args, config, fs, logger){
+	initialize:function(config){
 		// cmd line args
-		for(var i=0;i<args.length;i++){
-			var arg = args[i]
+		for(var i=0;i<config.process.argv.length;i++){
+			var arg = config.process.argv[i]
 			if('-r' == arg){
 				config.recursive = true
 			}else if('-s' == arg){
 				// suppress 'found' output
 				config.msgs.found_jar = null
-			}else if('-x' == arg && (args.length-1 > i)){
+			}else if('-x' == arg && (config.process.argv.length-1 > i)){
 				// set dependency log filename
-				fs.unlink(args[i+1])
-				logger.info = fs.createWriteStream(args[i+1], {'flags': 'w'})
-			}else if('-e' == arg && (args.length-1 > i)){
+				config.requires.fs.unlink(config.process.argv[i+1])
+				config.logger.info = config.fs.createWriteStream(args[i+1], {'flags': 'w'})
+			}else if('-e' == arg && (config.process.argv.length-1 > i)){
 				// set error log filename
-				fs.unlink(args[i+1])
-				logger.error = fs.createWriteStream(args[i+1], {'flags': 'w'})
+				config.requires.fs.unlink(config.process.argv[i+1])
+				config.logger.error = fs.createWriteStream(config.process.argv[i+1], {'flags': 'w'})
 			}	
 		}
+		config.emitter = new config.requires.events.EventEmitter();
 		return this
 	},
 	register_events:function(emitter){
 		// setup events
-		emitter.on('error', function(error){
-			jh.error_counter(error)
+		emitter.on('execute_on_dir', function(filename, config){
+			jh.execute(filename, config)
 		})
-		emitter.on('execute_on_dir', function(filename){
-			jh.execute(filename)
+		emitter.on('filter', function(qualified_filename, config){
+			jh.filter(qualified_filename, config)
 		})
-		emitter.on('filter', function(qualified_filename){
-			jh.filter(qualified_filename)
+		emitter.on('generate_hash', function(filename, config){
+			jh.generate_hash(filename, config)
 		})
-		emitter.on('generate_hash', function(filename){
-			jh.generate_hash(filename)	
+		emitter.on('generate_url', function(filename, hash, config){
+			jh.generate_url(filename, hash, config)
 		})
-		emitter.on('generate_url', function(filename, hash){
-			jh.generate_url(filename, hash)
+		emitter.on('search', function(filename, path, config){
+			jh.search(filename, path, config)
 		})
-		emitter.on('search', function(filename, path){
-			jh.search(filename, path)
+		emitter.on('write_dependency_xml', function(xml, config){
+			config.logger.info.write(xml)
 		})
-		emitter.on('write_dependency_xml', function(xml){
-			jh.state.logger.info.write(xml)
-		})
-		emitter.on('write_error_xml', function(error){
-			jh.state.logger.error.write(error)
+		emitter.on('write_error_xml', function(error, config){
+			config.logger.error.write(error)
 		})
 		return this
 	},
-	search:function(filename, path){
+	search:function(filename, path, config){
 		var options = {
-			'host':jh.config.http_options.host,
-			'port':jh.config.http_options.port,
+			'host':config.http_options.host,
+			'port':config.http_options.port,
 			'path':path
 		}
-		http.get(options, function(res) {  
+		config.requires.http.get(options, function(res) {  
 			res.on('data', function(chunk) {
 				try{
 					var obj = JSON.parse(chunk)
-					jh.config.emitter.emit('write_dependency_xml', util.format(jh.config.xml.template, obj.response.docs[0].g, obj.response.docs[0].a, obj.response.docs[0].v))
+					var value = config.requires.util.format(config.xml.template, obj.response.docs[0].g, obj.response.docs[0].a, obj.response.docs[0].v)
+					config.emitter.emit('write_dependency_xml', value, config)
 				}catch(e){
-					jh.config.emitter.emit('write_error_xml', util.format(jh.config.xml.error_template, filename, options.host, options.path))
+					var value = config.requires.util.format(config.xml.error_template, filename, options.host, options.path)
+					config.emitter.emit('write_error_xml', value, config)
 				}
 			})
 		}).on('error', function(error) {			
-			jh.config.emitter.emit('error',error)
+			config.console.log(error)
+			config.process.exit()
 		})		
 	},
-	validate_args:function(console, fs, messages, path, process, this_script){	
-		var last_arg = process.argv[process.argv.length-1]	
+	validate_args:function(config){
+		var last_arg = config.process.argv[config.process.argv.length-1]	
 		
 		// validate parameter count	
-		if(last_arg == this_script){
-			messages.help_text.forEach(function(text){
-			 	console.log(text)
+		if(last_arg == config.filenames.this_script){
+			config.msgs.help_text.forEach(function(text){
+			 	config.console.log(text)
 			})
-			process.exit()
+			config.process.exit()
 		}
 		
 		// validate directory was supplied and that it exists
-		if(path.existsSync(last_arg)){
-			var stat = fs.statSync(last_arg)
+		if(config.requires.path.existsSync(last_arg)){
+			var stat = config.requires.fs.statSync(last_arg)
 			if(!stat.isDirectory()){
-				console.log(util.format(messages.validation_not_directory, last_arg))
-				process.exit()
+				config.console.log(config.requires.util.format(config.msgs.validation_not_directory, last_arg))
+				config.requires.process.exit()
 			}
 		}else{
-			console.log(util.format(messages.validation_dir_ne, last_arg))
-			process.exit()
+			config.console.log(config.requires.util.format(config.msgs.validation_dir_ne, last_arg))
+			config.requires.process.exit()
 		}
 		return this
 	},
-	validate_loggers:function(default_filenames, fs, logger){
+	validate_loggers:function(config){
 		// default filenames?
-		if(!logger.error)
-			logger.error = fs.createWriteStream(default_filenames.error_xml, {'flags': 'w'})
-		if(!logger.info) 
-			logger.info = fs.createWriteStream(default_filenames.dependency_xml, {'flags': 'w'})			
+		if(!config.logger.error)
+			config.logger.error = config.requires.fs.createWriteStream(jh.config.filenames.error_xml, {'flags': 'w'})
+		if(!config.logger.info) 
+			config.logger.info = config.requires.fs.createWriteStream(jh.config.filenames.dependency_xml, {'flags': 'w'})			
 		return this	
 	}
 }
 
 jh
-	.validate_args(console, fs, jh.config.msgs, path, process)
-	.initialize(process.argv, jh.config, fs, jh.state.logger)
-	.validate_loggers(jh.config.filenames, fs, jh.state.logger)
+	.validate_args(jh.config)
+	.initialize(jh.config)
+	.validate_loggers(jh.config)
 	.register_events(jh.config.emitter)
-	.execute(process.argv[process.argv.length-1])
+	.execute(null, jh.config)
