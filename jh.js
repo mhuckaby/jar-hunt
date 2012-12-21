2d36ceaea95da3339710304ad97173315bf4e4b7
Matthew Huckaby <matthewhuckaby@Matthews-MacBook-Pro.local>
2012-12-20 19:58:26 -0600

    fixed formatting and command line processing bugs

--- jh.js
+++ jh.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*
-jar-hunt, version 1.0.2
+jar-hunt, version 1.0.3
 
 jh.js will generate <dependency> XML fragments for a directory of jar-files to be used in Maven pom.xml file.
 This XML fragment is created by recursively searching the specified directory for jar-files,
@@ -43,30 +43,29 @@ var jarhunt_context = {
       "console": console,
       "emitter": null,
       "filenames": {
-        "dependency_xml": 'dependency.xml',
-        "error_xml": 'error.xml',
+        "dependency_xml": "dependency.xml",
+        "error_xml": "error.xml",
         "this_script": __filename
       },
       "http_options": {
-        "host": 'search.maven.org',
-        "port": 80,
+        "host": "search.maven.org",
+        "port": 80
       },
       "logger": {
         "error": null,
         "info": null
       },
       "msgs": {
-        "found_jar": 'found \t: %s\nhash \t: %s',
-        "help_text": ['jar-hunt, v.1.0.0b',
+        "found_jar": "found \t: %s\nhash \t: %s",
+        "help_text": ['jar-hunt, v.1.0.3',
           'usage:',
-          '\t[node] jh.js [-s -x $filename -e $filename] ${directory_that_contains_jars}\n',
+          '\t[node] jh.js [-e {filename} -r -s -x {filename}] {directory_that_contains_jars}\n',
           '\t-e ${filename} to write error output, default is, "error.xml"',
           '\t-r recursively search for jar-files',
           '\t-s suppress "found" messages',
-          '\t-x ${filename} to write xml output, default is, "dependency.xml"',
-          ],
-        "validation_not_directory": 'argument, "%s" is not a directory',
-        "validation_dir_ne": 'directory, "%s" does not exist.'
+          '\t-x ${filename} to write xml output, default is, "dependency.xml"'],
+        "validation_not_directory": "argument, \"%s\" is not a directory:",
+        "validation_dir_ne": "directory, \"%s\" does not exist."
       },
       "process": process,
       "recursive": false,
@@ -79,27 +78,28 @@ var jarhunt_context = {
         "util": require('util')
       },
       "url": {
-        "param_template": ':"%s"',
-        "template": '/solrsearch/select?q=1%s&rows=20&wt=json'
+        "param_template": ":\"%s\"",
+        "template": "/solrsearch/select?q=1%s&rows=20&wt=json"
       },
       "xml": {
-        "error_template": '<error\nfile="%s"\nurl="%s%s" />\n',
+        "error_template": "<error\nfile=\"%s\"\nurl=\"%s%s\" />\n",
         "template":
-          '<dependency>\n' +
-          '\t<groupId>%s</groupId>\n' +
-          '\t<artifactId>%s</artifactId>\n' +
-          '\t<version>%s</version>\n' +
-          '</dependency>\n'
+          "<dependency>\n" +
+          "\t<groupId>%s</groupId>\n" +
+          "\t<artifactId>%s</artifactId>\n" +
+          "\t<version>%s</version>\n" +
+          "</dependency>\n"
       }
     };
   },
   "jarHuntInstance": function (config) {
-    var config = config || this.configInstance();
-    var private = {
-      "initialize": function() {
-        // cmd line args
+    config = config || this.configInstance();
+    var initialize = {
+      "handle_cmd_line_arguments": function() {
+        // cmd line arguments
         for(var i=0;i<config.process.argv.length;i++) {
           var arg = config.process.argv[i];
+
           if('-r' == arg) {
             config.recursive = true;
           }else if('-s' == arg) {
@@ -108,11 +108,13 @@ var jarhunt_context = {
           }else if('-x' == arg && (config.process.argv.length-1 > i)) {
             // set dependency log filename
             config.requires.fs.unlink(config.process.argv[i+1]);
-            config.logger.info = config.fs.createWriteStream(args[i+1], {'flags': 'w'});
+            config.logger.info =
+                config.requires.fs.createWriteStream(config.process.argv[i+1], {"flags": "w"});
           }else if('-e' == arg && (config.process.argv.length-1 > i)) {
             // set error log filename
             config.requires.fs.unlink(config.process.argv[i+1]);
-            config.logger.error = fs.createWriteStream(config.process.argv[i+1], {'flags': 'w'});
+            config.logger.error =
+                config.requires.fs.createWriteStream(config.process.argv[i+1], {"flags": "w"});
           }
         }
 
@@ -121,7 +123,7 @@ var jarhunt_context = {
       },
       "register_events": function(emitter) {
         emitter.on('execute', function(dir, config) {
-          var dir = dir ? dir : config.process.argv[config.process.argv.length-1];
+          dir = dir ? dir : config.process.argv[config.process.argv.length-1];
           config.requires.fs.readdir(dir, function(err, filenames) {
             if(filenames) {
               filenames.forEach(function(filename) {
@@ -132,7 +134,7 @@ var jarhunt_context = {
               config.console.log('no files found : ' + dir);
             }
           })
-        })
+        });
 
         emitter.on('filter', function(filename, config) {
           config.requires.fs.stat(filename, function(error, stats) {
@@ -144,7 +146,7 @@ var jarhunt_context = {
               }
             }
           })
-        })
+        });
 
         var queue;
         emitter.on('queue', function(filename, config) {
@@ -152,12 +154,12 @@ var jarhunt_context = {
             queue.push(filename);
           }else{
             // first
-            queue = new Array()
+            queue = [];
             emitter.emit('generate_hash_asynch', filename, config, function() {
               emitter.emit('chew-queue', config);
             })
           }
-        })
+        });
 
         emitter.on('chew-queue', function(config) {
           if(queue.length) {
@@ -165,7 +167,7 @@ var jarhunt_context = {
               emitter.emit('chew-queue', config);
             })
           }
-        })
+        });
 
         emitter.on('generate_hash_asynch', function(filename, config, post_read_callback) {
           config.requires.fs.readFile(filename, function(err, data) {
@@ -179,43 +181,56 @@ var jarhunt_context = {
 
             config.emitter.emit('generate_url', filename, hash, config);
           })
-        })
+        });
 
         emitter.on('generate_url', function(filename, hash, config) {
           var param = encodeURIComponent(config.requires.util.format(config.url.param_template, hash));
           config.emitter.emit('search', filename, config.requires.util.format(config.url.template, param), config);
-        })
+        });
 
         emitter.on('search', function(filename, path, config) {
           var options = {
-            'host':config.http_options.host,
-            'port':config.http_options.port,
-            'path':path
-          }
+            "host": config.http_options.host,
+            "port": config.http_options.port,
+            "path": path
+          };
+
           config.requires.http.get(options, function(res) {
             res.on('data', function(chunk) {
               try{
                 var obj = JSON.parse(chunk);
-                var value = config.requires.util.format(config.xml.template, obj.response.docs[0].g, obj.response.docs[0].a, obj.response.docs[0].v);
+                var value =
+                    config.requires.util.format(
+                        config.xml.template,
+                        obj.response.docs[0].g,
+                        obj.response.docs[0].a,
+                        obj.response.docs[0].v
+                    );
                 config.emitter.emit('write_dependency_xml', value, config);
               }catch(e) {
-                var value = config.requires.util.format(config.xml.error_template, filename, options.host, options.path);
-                config.emitter.emit('write_error_xml', value, config);
+                var error_message =
+                    config.requires.util.format(
+                        config.xml.error_template,
+                        filename,
+                        options.host,
+                        options.path
+                    );
+                config.emitter.emit('write_error_xml', error_message, config);
               }
             })
           }).on('error', function(error) {
             config.console.log(error);
             config.process.exit();
           })
-        })
+        });
 
         emitter.on('write_dependency_xml', function(xml, config) {
           config.logger.info.write(xml);
-        })
+        });
 
         emitter.on('write_error_xml', function(error, config) {
           config.logger.error.write(error);
-        })
+        });
 
         return this
       },
@@ -226,7 +241,8 @@ var jarhunt_context = {
         if(last_arg == config.filenames.this_script) {
           config.msgs.help_text.forEach(function(text) {
              config.console.log(text);
-          })
+          });
+
           config.process.exit();
         }
 
@@ -234,7 +250,7 @@ var jarhunt_context = {
         if(config.requires.fs.existsSync(last_arg)) {
           var stat = config.requires.fs.statSync(last_arg);
           if(!stat.isDirectory()) {
-            config.conssole.log(config.requires.util.format(config.msgs.validation_not_directory, last_arg));
+            config.console.log(config.requires.util.format(config.msgs.validation_not_directory, last_arg));
             config.requires.process.exit();
           }
         }else{
@@ -242,7 +258,7 @@ var jarhunt_context = {
           config.process.exit();
         }
 
-        return this
+        return this;
       },
       "validate_loggers": function() {
         if(!config.logger.error)
@@ -257,9 +273,9 @@ var jarhunt_context = {
 
     return {
       "execute": function() {
-        private
+        initialize
           .validate_args()
-          .initialize()
+          .handle_cmd_line_arguments()
           .validate_loggers()
           .register_events(config.emitter);
 
